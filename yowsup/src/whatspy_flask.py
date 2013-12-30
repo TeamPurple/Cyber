5b461b76930eac6a289aaae3a3d56c8a023a3b61
Eric Pelz <eric+github@ericpelz.com>
2013-12-30 16:50:59 +0200

    Cleaned up whatspy_flask
    - Removed unnecessary code
    - Timeout if no picture or last update time

--- yowsup/src/whatspy_flask.py
+++ yowsup/src/whatspy_flask.py
@@ -1,4 +1,4 @@
-import sys, os, time, shutil, argparse
+import sys, os, time, shutil
 
 from Yowsup.connectionmanager import YowsupConnectionManager
 from Yowsup.Common.debugger import Debugger
@@ -10,7 +10,6 @@ phase = None
 cm = None
 signals_interface = None
 methods_interface = None
-args = None
 
 # Utils
 def phone_number2jid(phone_number):
@@ -33,9 +32,6 @@ def login():
     methods_interface.call('auth_login', (USERNAME, PASSWORD))
 
 # Presence
-def cb_presence_updated(jid, last_seen):
-    print 'Last seen @', time.ctime(time.time() - last_seen)
-
 def cb_presence_updated_once(jid, last_seen):
     ''' TODO: save the time to something and then use the web app to load it'''
     global time_got
@@ -47,17 +43,12 @@ def cb_presence_updated_once(jid, last_seen):
 def cb_contact_gotProfilePicture_once(jid, picture_id, image_path):
     ''' for eric to use for web app
     TODO: modify the path so that it goes to where you want
-    TODO: modify a global variable so it knows its done'''
+    '''
     global photo_got
     phone_number = jid.split('@')[0]
     print 'Got', phone_number
-    # shutil.copyfile(image_path, os.path.join(args.t, phone_number + '.jpg'))
-    photo_got = True
-
-def cb_contact_gotProfilePicture_batch(jid, picture_id, image_path):
-    phone_number = jid.split('@')[0]
-    print 'Got', phone_number
     shutil.copyfile(image_path, os.path.join(args.t, phone_number + '.jpg'))
+    photo_got = True
 
 # Misc
 def cb_disconnected(reason):
@@ -83,24 +74,6 @@ def setup():
     signals_interface.registerListener('auth_fail', cb_auth_fail)
     signals_interface.registerListener('disconnected', cb_disconnected)
 
-def batch(phone_numbers_path, profiles_picture_path):
-    signals_interface.registerListener('contact_gotProfilePicture', cb_contact_gotProfilePicture_batch)
-    
-    with open(phone_numbers_path, 'rb') as f:
-        phone_numbers = f.read().split('\n')
-
-    if not os.path.exists(profiles_picture_path):
-        os.mkdir(profiles_picture_path)
-
-    for phone_number in phone_numbers:
-        jid = phone_number2jid(phone_number)
-
-        print 'Request', phone_number
-        methods_interface.call('contact_getProfilePicture', (jid,))
-        time.sleep(2)
-
-    time.sleep(10)
-
 def get_photo_time(phone_number):
     ''' phone number includes the country code
     '''
@@ -120,65 +93,15 @@ def get_photo_time(phone_number):
 
     methods_interface.call('contact_getProfilePicture', (jid,))
     
-    while not (photo_got and time_got):
+    timeout = 0
+    while not (photo_got and time_got) and timeout < 1:
         #TODO: Time out the request for both photo and time depending on whats available
         print photo_got, time_got
-        time.sleep(0.5)
+        time.sleep(0.25)
+        timeout += 0.25
             
-    methods_interface.call('disconnect', ('closed!',))
+#    methods_interface.call('disconnect', ('closed!',))
         
-def interactive():
-    signals_interface.registerListener('presence_updated', cb_presence_updated)
-    signals_interface.registerListener('contact_gotProfilePicture', cb_contact_gotProfilePicture_once)
-    
-    try:
-        while True:
-            cmd = raw_input('>>> ')
-            
-            if cmd == 'exit':
-                break
-
-            elif cmd.isdigit():
-                jid = phone_number2jid(cmd)
-                methods_interface.call('presence_request', (jid,))
-                methods_interface.call('contact_getProfilePicture', (jid,))
-
-            elif cmd == '':
-                continue
-                
-            else:
-                print 'Not a valid number...'
-
-            time.sleep(2)
-
-    except KeyboardInterrupt:
-        print
-
-parser = argparse.ArgumentParser()
-mode_subparsers = parser.add_subparsers(dest='mode', help='choose operation mode')
-
-interactive_parser = mode_subparsers.add_parser('interactive', help='interactive phone number query of profile picture & last seen')
-
-batch_parser = mode_subparsers.add_parser('batch', help='batch phone numbers query of profile picture')
-batch_parser.add_argument('-p', required=True, help='phone numbers file (seperated by new line')
-batch_parser.add_argument('-t', required=True, help='profiles picture directory')
-
-args = parser.parse_args()
 
 get_photo_time('16094755004')
-
-# setup()
-
-# login()
-
-# while phase is None:
-#     time.sleep(0.5)
-
-# if phase:
-#     if args.mode == 'interactive':
-#         interactive()
-
-#     elif args.mode == 'batch':
-#         batch(args.p, args.t)
-    
-# methods_interface.call('disconnect', ('closed!',))
+get_photo_time('16094755004')
