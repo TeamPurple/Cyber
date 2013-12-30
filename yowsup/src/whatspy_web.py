49cf01fcd6b36af1ec33609a10b273e2110a788f
Max Kleiman-Weiner <maxkw@mit.edu>
2013-12-30 13:32:52 +0200

    updated for simple numbers

--- yowsup/src/whatspy_web.py
+++ yowsup/src/whatspy_web.py
@@ -40,12 +40,14 @@ def cb_auth_fail(username, reason):
     phase = False
 
 def login():
+    print 'Logging in'
     methods_interface.call('auth_login', (USERNAME, PASSWORD))
 
 # Contacts
 
 def cb_contact_gotProfilePicture(jid, picture_id, image_path):
     global phase, profile_picture_path
+    print 'image_path', image_path
     profile_picture_path = image_path
     phase = True
 
@@ -60,10 +62,10 @@ def cb_message_sent(jid, message_id):
 # Main
 
 def setup():
-
+    Debugger.enabled = False    
     global cm, signals_interface, methods_interface
+    print 'Settng up environment'
 
-    Debugger.enabled = False
 
     cm = YowsupConnectionManager()
 
@@ -78,14 +80,45 @@ def setup():
     signals_interface.registerListener('receipt_messageSent', cb_message_sent)
     signals_interface.registerListener('receipt_messageDelivered', cb_message_delivered)
 
+    # Presence
+    signals_interface.registerListener('presence_updated', cb_presence_updated)
+    # signals_interface.registerListener('presence_available', cb_presence_updated)
+    # signals_interface.registerListener('presence_unavailable', cb_presence_updated)
+    
+def cb_presence_updated(last_seen):
+    """ callback for when presence is updated
+    """
+    print 'presence_updated', last_seen
 
+def last_online(phone_number):
+    """
+    """
+    global profile_picture_path, phase
+    profile_picture_path = None
+    jid = phone_number2jid(phone_number)
+    phase = None
+    print 'Calling presence_requests'
+    # First set presence available for myself
 
+    methods_interface.call('presence_sendAvailable')
+
+    # methods_interface.call('presence_subscribe', (jid,))
+    # methods_interface.call('presence_subscribe', (jid,))
+
+    print methods_interface.call('presence_request', (jid,))    
+
+    print 'Called'
+    while phase is None:
+        time.sleep(0.5)
+    
 def generate_profile_picture(phone_number):
     global profile_picture_path, phase
     profile_picture_path = None
     jid = phone_number2jid(phone_number)
     phase = None
+    print 'Calling contact_getProfilePicture'
     methods_interface.call('contact_getProfilePicture', (jid,))
+    print 'Called'
     while phase is None:
         time.sleep(0.5)
 
@@ -101,16 +134,23 @@ run()
 # Flask #
 #########
 
-from flask import Flask, request, send_file
+# from flask import Flask, request, send_file
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/whatspy/<phone_number>')
-def whatspy(phone_number):
+# @app.route('/whatspy/<phone_number>')
+# def whatspy(phone_number):
+#     global profile_picture_path
+#     generate_profile_picture(phone_number)
+#     # send_debug(request.remote_addr + ' :: ' + phone_number)
+#     return send_file(profile_picture_path, mimetype='image/jpeg')
+
+if __name__ == '__main__':
+    # app.run(debug=True, port=5000)
     global profile_picture_path
-    generate_profile_picture(phone_number)
-    send_debug(request.remote_addr + ' :: ' + phone_number)
-    return send_file(profile_picture_path, mimetype='image/jpeg')
-
-#if __name__ == '__main__':
-#    app.run(debug=True, port=5000)
+    phone_number = '13104874553'
+    # generate_profile_picture(phone_number)
+    last_online(phone_number)
+    # send_debug(request.remote_addr + ' :: ' + phone_number)
+    # return send_file(profile_picture_path, mimetype='image/jpeg')
+    
