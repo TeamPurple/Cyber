364fc806f69efe3153e56e8c06d8f9b411e7815c
Jacob Hurwitz <jhurwitz@mit.edu>
2013-12-30 20:27:41 +0200

    change reverse_image initialization to be lazy

--- webapp.py
+++ webapp.py
@@ -3,7 +3,7 @@ import whatspy
 app = Flask(__name__)
 
 from reverse_image import ReverseImageSearcher
-image_searcher = ReverseImageSearcher() # TODO possibly move this elsewhere
+image_searcher = None
 
 @app.route('/')
 def index():
@@ -17,12 +17,16 @@ def view():
 
 @app.route('/reverse_image/<local_image_path>')
 def reverse_image(local_image_path):
-	endpoint_url = url_for("get_reverse_image_results", local_image_path=local_image_path)
-	return render_template('reverse_image.html', endpoint_url=endpoint_url)
+    endpoint_url = url_for("get_reverse_image_results", local_image_path=local_image_path)
+    return render_template('reverse_image.html', endpoint_url=endpoint_url)
 
 @app.route('/get_reverse_image_results/<local_image_path>')
 def get_reverse_image_results(local_image_path):
-	return image_searcher.get_results(local_image_path)
+    global image_searcher
+    if image_searcher is None:
+        image_searcher = ReverseImageSearcher()
+
+    return image_searcher.get_results(local_image_path)
 
 # Fire up the server
 if __name__ == '__main__':
