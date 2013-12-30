bba0ed94ace56ad52ad26f13553fe1998f81f07f
Eric Pelz <eric+github@ericpelz.com>
2013-12-30 17:57:20 +0200

    Can fetch image via web
    Fetches WhatsApp image in Flask and then passes
    the image path to the template.

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import whatspy
 app = Flask(__name__)
 
 
@@ -6,7 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('purple.html')
 
-
+@app.route('/view')
+def view():
+    # note: photo_path/last_time can be None
+    photo_path, last_time = whatspy.get_photo_time('13104874553')
+    return render_template('results.html', photo_path=photo_path)
 
 # Fire up the server
 if __name__ == '__main__':
