308185868f28c184b33dc8dbae6fb07f496ee1f6
Jacob Hurwitz <jhurwitz@mit.edu>
2013-12-31 05:05:24 +0200

    Proof-of-concept for loading data from multiple sources
    The previous version of the site hit one endpoint in Python, then only after
    receiving this data, loaded the proper HTML template. This is bad -- if you're
    hitting multiple endpoints, you need to wait for them all to return before
    showing anything!
    
    I changed the page to initially load with no data (just loading
    spinners/indicators), then use AJAX to fill in each part as the endpoint
    returns the data.
    
    This is just a proof of concept. It's ugly and there are tons of bugs (namely,
    if an endpoint fails to return data, I don't check/handle that properly).
    However, I think it's a good base to build off of.

--- webapp.py
+++ webapp.py
@@ -5,6 +5,7 @@ import requests, json
 app = Flask(__name__)
 
 import reverse_image
+import facebook
 
 from config import AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_BUCKET
 from boto.s3.connection import S3Connection
@@ -17,9 +18,14 @@ def index():
 def view():
     if 'phone_number' not in request.form:
         return redirect(url_for('index'))
-
-    whatspy = Whatspy()
     phone_number = request.form['phone_number']
+
+    return render_template('results.html', phone_number=phone_number)
+
+@app.route('/ajax/whatsapp', methods=['POST'])
+def whatsapp_endpoint():
+    phone_number = request.form['phone_number']
+    whatspy = Whatspy()
 
     # note: photo_path/last_time can be None
     photo_path, last_time = whatspy.get_photo_time(phone_number)
@@ -28,12 +34,15 @@ def view():
     if last_time:
       last_time = datetime.strptime(last_time, "%a %b %d %H:%M:%S %Y")
 
-    return render_template('results.html', photo_path=photo_path, last_time=last_time, phone_number=phone_number)
+    data = dict(
+        photo_path = photo_path,
+        last_time = str(last_time),
+    )
+    return json.dumps(data)
 
-@app.route('/reverse_image/<local_image_path>')
+@app.route('/demo/reverse_image/<local_image_path>')
 def reverse_image_tester(local_image_path):
-    endpoint_url = url_for("get_reverse_image_results", local_image_path=local_image_path)
-    return render_template('reverse_image.html', endpoint_url=endpoint_url)
+    return render_template('reverse_image.html', local_image_path=local_image_path)
 
 s3_bucket = None
 
@@ -44,8 +53,9 @@ def get_s3_bucket():
         s3_bucket = conn.get_bucket(AWS_BUCKET)
     return s3_bucket
 
-@app.route('/get_reverse_image_results/<local_image_path>')
-def get_reverse_image_results(local_image_path):
+@app.route('/ajax/reverse_image', methods=['POST'])
+def reverse_image_endpoint():
+    local_image_path = request.form['local_image_path']
     bucket = get_s3_bucket()
     return reverse_image.get_results(local_image_path, bucket)
 
@@ -54,6 +64,12 @@ def timeline():
     phone = request.form['phone']
     r = requests.get('http://50.112.143.163/?phone=' + phone)
     return jsonify(json.loads(r.text))
+
+@app.route('/ajax/facebook', methods=['POST'])
+def facebook_endpoint():
+    phone_number = request.form['phone_number']
+    fbid = facebook.lookup_facebook_by_phone(phone_number)
+    return str(fbid)
 
 # Fire up the server
 if __name__ == '__main__':
